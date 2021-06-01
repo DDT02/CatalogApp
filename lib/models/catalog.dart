7eaccdc4978b89fabc263587fddfe2eefe3af337
DDT02 <deveshdutt_tripathi.scsebtech@galgotiasuniversity.edu.in>
2021-06-01 12:50:51 +0530

    day 11 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,19 @@
+class CatalogModel {
+  static final items = [
+    Item(
+      id: 1,
+      name: "iphone 12 pro",
+      desc: "Apple Iphone 12th Generation",
+      price: 999,
+      color: "#33505a",
+      image:
+          "https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-12-pro-r1.jpg",
+    )
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
